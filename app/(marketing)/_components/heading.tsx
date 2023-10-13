0c300dcbf59275f2654b1fef36a9b02a29d965c4
seo722 <isogood11@naver.com>
2023-10-13 16:47:09 +0900

    authentication & convex database

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,16 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { useConvexAuth } from 'convex/react';
 import { ArrowRight } from 'lucide-react';
+import { SignInButton } from '@clerk/clerk-react';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/spinner';
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,10 +20,25 @@ export const Heading = () => {
         Seotion is the connected workspace where <br />
         better,faster work happens.
       </h3>
-      <Button>
-        Enter Seotion
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button>
+          <Link href="/documents">Enter Seotion</Link>
+          <ArrowRight className="h-4 w-4 ml-2" />
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Seotion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://direct-marlin-69.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
